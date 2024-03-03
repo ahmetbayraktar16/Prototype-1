87210556962c77112b4fc86f9192a0f2d4f8faaf
Ahmet Bayraktar <apron4x@gmail.com>
2024-03-04 00:15:39 +0300

    Added obstacles, oncoming vehicles, and camera switcher; implemented physics engine and movement with transform.Translate.

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
+    public Camera mainCamera;
+    public Camera sideCamera;
     public GameObject player;
-    private Vector3 offset = new Vector3 (0, 5, -7);
+    private Vector3 mainOffset = new Vector3 (0, 5, -7);
+    private Vector3 sideOffset = new Vector3 (0, 2, 1.5f);
 
     // Start is called before the first frame update
     void Start()
@@ -13,10 +16,27 @@ public class FollowPlayer : MonoBehaviour
         
     }
 
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.V))
+        {
+            if(mainCamera.gameObject.activeSelf)
+            {
+                mainCamera.gameObject.SetActive(false);
+                sideCamera.gameObject.SetActive(true);
+            }
+            else
+            {
+                mainCamera.gameObject.SetActive(true);
+                sideCamera.gameObject.SetActive(false);
+            }
+        }
+    }
     // Update is called once per frame
     void LateUpdate()
     {
         //Offset the camera behind the player by adding to the player's position
-        transform.position = player.transform.position + offset;
+        mainCamera.transform.position = player.transform.position + mainOffset;
+        sideCamera.transform.position = player.transform.position + sideOffset;
     }
 }
